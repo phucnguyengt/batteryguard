7d1d8faf57a87a8416d4d581bc87e3945918e976
phucnguyengt <phucnguyengt@gmail.com>
2026-08-19 23:12:59 +0700

    Set Java and Kotlin target to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,11 @@ android {
     debug { }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
   }
   buildFeatures {
     compose = true
